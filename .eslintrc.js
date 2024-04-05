3fa345ce956856333109a3330bf33d4c0ecad853
avasuro <avasuro.dev@gmail.com>
2024-04-05 12:22:59 +0200

    build: updated libs versions + added typescript to src files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,22 @@
 /* eslint-env commonjs */
+const avasuroEslint = require('eslint-config-avasuro');
+const {INDENT, QUOTES, PLUGINS} = require('eslint-config-avasuro/constants');
+
 module.exports = {
     extends: [
-        'eslint-config-airbnb/rules/react',
-        'eslint-config-airbnb/rules/react-hooks',
-        'eslint-config-avasuro/2__single_quotes_and_spaces'
+        avasuroEslint({
+            config: {
+                indent: INDENT.SPACES_4,
+                quotes: QUOTES.SINGLE
+            },
+            plugins: [
+                PLUGINS.CORE,
+                PLUGINS.REACT,
+                PLUGINS.TYPESCRIPT
+            ]
+        })
     ],
     env: {
         browser: true
-    },
-    rules: {
-    // Validate props indentation in JSX
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
-        'react/jsx-indent-props': ['error', 4],
-        // Enforce JSX indentation
-        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
-        'react/jsx-indent': ['error', 4],
-        // only .jsx and .js files may have JSX
-        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-        'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.js']}],
-        // Enforce state initialization style
-        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/state-in-constructor.md
-        'react/state-in-constructor': ['error', 'never']
     }
 };
